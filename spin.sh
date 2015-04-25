ac9c9595b64b1d98ae5fc5d09976b26d85d942db
Dan Book <grinnz@grinnz.com>
2015-04-24 20:45:04 -0400

    update labels to F22

--- spin.sh
+++ spin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TODAY=$(date +%Y%m%d)
-sudo livecd-creator --verbose --config=fedora-livecd-cinnamon.ks --fslabel=F21Live-Cinnamon-x86_64-$TODAY --cache=/var/cache/live
+sudo livecd-creator --verbose --config=fedora-livecd-cinnamon.ks --fslabel=F22Live-Cinnamon-x86_64-$TODAY --cache=/var/cache/live
--- spin32.sh
+++ spin32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TODAY=$(date +%Y%m%d)
-sudo setarch i686 livecd-creator --verbose --config=fedora-livecd-cinnamon.ks --fslabel=F21Live-Cinnamon-i686-$TODAY --cache=/var/cache/live
+sudo setarch i686 livecd-creator --verbose --config=fedora-livecd-cinnamon.ks --fslabel=F22Live-Cinnamon-i686-$TODAY --cache=/var/cache/live
